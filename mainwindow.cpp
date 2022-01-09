e0733f61a2d8a66696519b431f688d2d48d54b96
1217268470 <xxx@xxx.com>
2022-01-09 11:41:57 +0800

    add

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -11,4 +11,37 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::init(){
+    ui->tableWidget->setColumnCount(3);
+    ui->tableWidget->setRowCount(10);
+    QStringList header;
+    header.append("�½���");
+    header.append("URL");
+    header.append("����");
+//    header<<"�½���"<<"URL"<<"����";
+    ui->tableWidget->setHorizontalHeaderLabels(header);
+    ui->tableWidget->horizontalHeader()->setDefaultAlignment(Qt::AlignCenter);
+
+    ui->tableWidget->setShowGrid(true);
+    ui->tableWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    //��ֱ�����������ƶ�
+    ui->tableWidget->setVerticalScrollMode(QAbstractItemView::ScrollPerItem);
+    //ȥ���Զ�����
+    ui->tableWidget->setAutoScroll(false);
+
+    QTableWidgetItem *ex = new QTableWidgetItem();
+    ex->setBackground(QBrush(QColor(Qt::lightGray)));
+    ex->setText("��һ��");
+    ui->tableWidget->setItem(1,0,ex);
+
+    QTableWidgetItem *exurl = new QTableWidgetItem();
+    exurl->setText("xxxxxxxxxxxxxxxxxxx");
+    exurl->setBackground(QBrush(QColor(Qt::lightGray)));
+    ui->tableWidget->setItem(1,1,exurl);
+
+    QTableWidgetItem *exopt = new QTableWidgetItem();
+    exopt->setText("sc");
+    exopt->setBackground(QBrush(QColor(Qt::lightGray)));
+    ui->tableWidget->setItem(1,2,exopt);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,4 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void init();
 
 private:
     Ui::MainWindow *ui;
